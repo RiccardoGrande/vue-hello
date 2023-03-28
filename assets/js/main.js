4635cfe2d1a7a5b3248c27120a294358948def07
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-28 14:56:42 +0200

    start commit push

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+//Stampare a schermo un messaggio all’interno di un :puntare_a_destra: h1 :puntare_a_sinistra: utilizzando una proprietá nell'oggetto restituito dalla funzione data.
+//Bonus:
+//Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+const { createApp } = Vue
+
+createApp({
+    /* option object */
+  data() {
+    return {
+      message: 'Hello Vue!',
+      contentStyle: ''
+    }
+  }
+}).mount('#app')
+
